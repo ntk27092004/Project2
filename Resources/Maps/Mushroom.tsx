4ae6d1ec04579dbe3592fe3bce7060b21f0b84e1
ntk27092004 <124237916+ntk27092004@users.noreply.github.com>
2024-01-29 16:42:21 +0700

    Add files via upload

---
+++ Resources/Maps/Mushroom.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mushroom" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../craftpix-891169-platformer-game-kit-pixel-art/platformer-pixel-art-cave-2d-tileset/Objects/Giant_Mushrooms/1.png" width="96" height="96"/>
+</tileset>
---
+++ Resources/Maps/Trap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Trap" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../../../craftpix-891169-platformer-game-kit-pixel-art/platformer-pixel-art-cave-2d-tileset/Objects/Rocks/3.png" width="32" height="32"/>
+</tileset>
